303a5920c6d9ee71e3c2f0813beb64641d9df117
yogindersingh658 <64410164+yogindersingh658@users.noreply.github.com>
2020-05-21 14:49:38 +0530

    done

--- app/src/main/java/com/example/doctorconsultantapp/AdminHome.java
+++ app/src/main/java/com/example/doctorconsultantapp/AdminHome.java
@@ -2,7 +2,9 @@ package com.example.doctorconsultantapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminHome extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class AdminHome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
+    }
+
+    public void go(View view)
+    {
+        Intent intent = new Intent(this,AdminChangePasswordActivity.class);
+        startActivity(intent);
     }
 }
